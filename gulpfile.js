c39a27ee56200ddc7f8627cc43f717220bcd86bc
Carla Akune <contato.cakune@gmail.com>
2024-03-06 22:54:30 -0300

    compilaSass adicionado e alteração no gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require ('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const obfuscate = require('gulp-obfuscate');
 const imagemin = require('gulp-imagemin');
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
 
 function comprimeImagens() {
     return gulp.src('./source/images/*')
@@ -18,6 +29,7 @@ function comprimeJavaScript() {
 }
 
 exports.default = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
     gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
     gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
 }
